1bf5bec0d9ec1daa3dbf0bd08a4f8e330054faff
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-25 15:27:34 +0000

    add AppEngine class

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-// Classes
-const Employee = require("./lib/employees/Employee.js");
-const Engineer = require("./lib/employees/Engineer.js");
-const Intern = require("./lib/employees/Intern.js");
-const Manager = require("./lib/employees/Manager.js");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// Node modules
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const render = require("./src/page-template.js");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const inquirer = require("inquirer");
+// const path = require("path");
+// const fs = require("fs");
 
-const render = require("./src/page-template.js");
+const AppEngine = require("./lib/engine/AppEngine");
 
 // TODO: Write Code to gather information about the development team members
 
 // TODO additional (not requirement) - consider adding validation to ensure that user input is in the proper format.
 
 // TODO: render the HTML file.
+
+AppEngine.displayWelcomeMsg();
+AppEngine.mainMenu();
