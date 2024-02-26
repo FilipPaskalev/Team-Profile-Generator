cccb8e0af29aea04d9aaf5c2e4a295abe702e9fd
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-26 13:05:05 +0000

    clean the file

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
-// Package
-const path = require('path');
-const fs = require('fs');
-
-// Constants
-const OUTPUT_DIR = path.resolve(__dirname, 'output');
-const outputPath = path.join(OUTPUT_DIR, 'team.html');
-
-// Functions
-const render = require('./src/page-template.js');
-
 // Classes
 const AppEngine = require('./lib/engine/AppEngine');
 
-// Create the output directory if the output path does not exist
-if (!fs.existsSync(OUTPUT_DIR)) {
-  fs.mkdirSync(OUTPUT_DIR);
-}
-
 // Create a new instance of the AppEngine
 const appEngine = AppEngine.getInstance();
+
+appEngine.prepareOutputDir();
 
 // Start the app
 appEngine.startTerminalApp();
