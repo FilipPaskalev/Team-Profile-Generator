dcc3c6761dae4f8252082877e2a2b4869289de3b
FilipPaskalev <77507389+FilipPaskalev@users.noreply.github.com>
2024-02-26 12:38:49 +0000

    create dir if is not exist

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// Package
+const path = require('path');
+const fs = require('fs');
 
-// const render = require("./src/page-template.js");
+// Constants
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
+// Functions
+const render = require('./src/page-template.js');
 
-// TODO: Write Code to gather information about the development team members
-
-// TODO additional (not requirement) - consider adding validation to ensure that user input is in the proper format.
-
-// TODO: render the HTML file.
-
+// Classes
 const AppEngine = require('./lib/engine/AppEngine');
 
+// Create the output directory if the output path does not exist
+if (!fs.existsSync(OUTPUT_DIR)) {
+  fs.mkdirSync(OUTPUT_DIR);
+}
+
+// Create a new instance of the AppEngine
 const appEngine = AppEngine.getInstance();
 
+// Start the app
 appEngine.start();
